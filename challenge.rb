6a5f3396cb2c198f5b7d902003b65c76e1e4702d
James O'Dwyer <j.odwyer320@gmail.com>
2014-09-16 00:29:37 -0400

    Pushed up with error in Capitalize method

--- challenge.rb
+++ challenge.rb
@@ -1,40 +1,40 @@
 
 def capitalize_each_string(input)
-  #implement your solution here
+  input.each { |animal|  animal.capitalize! }
 end
 
 def fetch_the_dog(input)
-  #implement your solution here
+  input.select { |animal| animal == "dog" }
 end
 
 def no_dogs_allowed(input)
-  #implement your solution here
+  input.select { |animal| animal != "dog" }
 end
 
 def count_the_animals(input)
-  #implement your solution here
+  input.count
 end
 
 def fetch_the_first_two(input)
-  #implement your solution here
+input.first(2)
 end
 
 def fetch_CD_animals(input)
-  #implement your solution here
+  input.select { |animal| animal.length == 3 }
 end
 
 ## DO NOT CHANGE CODE BELOW THIS LINE ##
 
 animals = ["cat", "moose", "dog", "bird"]
 
-p capitalize_each_string(animals) == ["Cat", "Moose", "Dog", "Bird"]
+p capitalize_each_string(animals) #== ["Cat", "Moose", "Dog", "Bird"]
 
-p fetch_the_dog(animals) == ["dog"] 
+p fetch_the_dog(animals) #== ["dog"] 
 
-p no_dogs_allowed(animals) == ["cat", "moose", "bird"]
+p no_dogs_allowed(animals) #== ["cat", "moose", "bird"]
 
-p count_the_animals(animals) == 4
+p count_the_animals(animals) #== 4
 
-p fetch_the_first_two(animals)  == ["cat", "moose"]
+p fetch_the_first_two(animals)  #== ["cat", "moose"]
 
-p fetch_CD_animals(animals) == ["cat", "dog"]
+p fetch_CD_animals(animals) #== ["cat", "dog"]
