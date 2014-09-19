cc366acd4179cbc0b4c11f35b620980a6de07ba7
Vasanth Pappu <vpappu@covermymeds.com>
2014-09-18 20:58:20 -0400

    my solutions

--- challenge.rb
+++ challenge.rb
@@ -1,26 +1,39 @@
-
 def capitalize_each_string(input)
   #implement your solution here
+  input.map do |w|
+    w.capitalize
+  end
 end
 
 def fetch_the_dog(input)
   #implement your solution here
+  input.partition do |w|
+    w == "dog"
+  end.first
 end
 
 def no_dogs_allowed(input)
   #implement your solution here
+  input.select do |w|
+    w != "dog"
+  end
 end
 
 def count_the_animals(input)
   #implement your solution here
+  input.size
 end
 
 def fetch_the_first_two(input)
   #implement your solution here
+  input[0..1]
 end
 
 def fetch_CD_animals(input)
   #implement your solution here
+  input.find_all do |w|
+    w =~ /^(c|d)/
+  end
 end
 
 ## DO NOT CHANGE CODE BELOW THIS LINE ##
