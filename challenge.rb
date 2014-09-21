0ff66852b0d36133de27d5f0b68477faacd88502
Brian Lees <blees1156@gmail.com>
2014-09-21 16:32:07 -0400

    solutions

--- challenge.rb
+++ challenge.rb
@@ -1,26 +1,26 @@
 
 def capitalize_each_string(input)
-  #implement your solution here
+  input.map{ |string| string.capitalize}
 end
 
 def fetch_the_dog(input)
-  #implement your solution here
+  input.select{ |animal| animal == 'dog' }
 end
 
 def no_dogs_allowed(input)
-  #implement your solution here
+  input.reject{ |animal| animal == 'dog' }
 end
 
 def count_the_animals(input)
-  #implement your solution here
+  input.count
 end
 
 def fetch_the_first_two(input)
-  #implement your solution here
+  input.take(2)
 end
 
 def fetch_CD_animals(input)
-  #implement your solution here
+  input.grep(/^(c|d)/)
 end
 
 ## DO NOT CHANGE CODE BELOW THIS LINE ##
