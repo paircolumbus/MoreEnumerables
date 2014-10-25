1616b61ec93d1204a147f69bce06b5714ff89396
Benjamin Madueme <bmadueme@gmail.com>
2014-10-25 13:13:24 -0400

    Implemented the empty methods in the challenge.

--- challenge.rb
+++ challenge.rb
@@ -1,26 +1,26 @@
 
 def capitalize_each_string(input)
-  #implement your solution here
+  input.map{ |element| element.capitalize}
 end
 
 def fetch_the_dog(input)
-  #implement your solution here
+  input.grep(/dog/)
 end
 
 def no_dogs_allowed(input)
-  #implement your solution here
+  input.select{ |x| x != "dog"}
 end
 
 def count_the_animals(input)
-  #implement your solution here
+  input.count
 end
 
 def fetch_the_first_two(input)
-  #implement your solution here
+  input.first(2)
 end
 
 def fetch_CD_animals(input)
-  #implement your solution here
+  input.grep(/^[cd].*$/i)
 end
 
 ## DO NOT CHANGE CODE BELOW THIS LINE ##
