07f30325e503b58915e1417e5c146b5e5132fb32
Peter Mueller <pmueller@covermymeds.com>
2015-05-22 12:41:10 -0400

    complete method logic

--- challenge.rb
+++ challenge.rb
@@ -1,26 +1,26 @@
 
 def capitalize_each_string(input)
-  #implement your solution here
+  input.map { |string| string.capitalize if string.class == String }
 end
 
 def fetch_the_dog(input)
-  #implement your solution here
+  input.select { |entry| entry.downcase == "dog" }
 end
 
 def no_dogs_allowed(input)
-  #implement your solution here
+  input.select { |entry| entry.downcase != "dog" }
 end
 
 def count_the_animals(input)
-  #implement your solution here
+  input.count
 end
 
 def fetch_the_first_two(input)
-  #implement your solution here
+  input.take(2)
 end
 
 def fetch_CD_animals(input)
-  #implement your solution here
+  input.grep(/^[c,d]/i)
 end
 
 ## DO NOT CHANGE CODE BELOW THIS LINE ##
@@ -29,7 +29,7 @@ animals = ["cat", "moose", "dog", "bird"]
 
 p capitalize_each_string(animals) == ["Cat", "Moose", "Dog", "Bird"]
 
-p fetch_the_dog(animals) == ["dog"] 
+p fetch_the_dog(animals) == ["dog"]
 
 p no_dogs_allowed(animals) == ["cat", "moose", "bird"]
 
