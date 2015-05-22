9903857f0eb7caf4cdf56fba1e98d9965baf30c6
Lovinder Pnag <lpnag@CMDVMOSXLT075.local>
2015-05-22 17:59:26 -0400

    finished challenge just need to remeber to use dup when needed

--- challenge.rb
+++ challenge.rb
@@ -1,26 +1,33 @@
 
 def capitalize_each_string(input)
-  #implement your solution here
+	input.map{|x| x.capitalize}
 end
 
 def fetch_the_dog(input)
-  #implement your solution here
+  	y = []
+  	input.each{|x| y << x if x == "dog"}
+  	y
 end
 
 def no_dogs_allowed(input)
-  #implement your solution here
+	a = input.dup
+  	a.delete("dog")
+  	a
 end
 
 def count_the_animals(input)
-  #implement your solution here
+  	input.count
 end
 
 def fetch_the_first_two(input)
-  #implement your solution here
+	a = input.dup
+	a.shift(2)
 end
 
 def fetch_CD_animals(input)
-  #implement your solution here
+	y = []
+	input.each{|x| y << x if x == "dog" || x == "cat"}
+	y
 end
 
 ## DO NOT CHANGE CODE BELOW THIS LINE ##
@@ -38,3 +45,4 @@ p count_the_animals(animals) == 4
 p fetch_the_first_two(animals)  == ["cat", "moose"]
 
 p fetch_CD_animals(animals) == ["cat", "dog"]
+p fetch_CD_animals(animals)
