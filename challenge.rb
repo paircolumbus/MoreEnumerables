29a84dd0267b39a2b350486565aaa4a1a26d4b79
Priya <priya.ramesh6@gmail.com>
2016-01-11 10:48:10 -0500

    Completed

--- challenge.rb
+++ challenge.rb
@@ -1,29 +1,67 @@
 
 def capitalize_each_string(input)
   #implement your solution here
+  final_result = []
+  intermediate = []
+  input.each do |i|
+  	 intermediate = i.split(//)
+     intermediate[0] = intermediate[0].upcase
+     result = intermediate.join
+   	 final_result.push(result)
+  end
+  return final_result
 end
 
 def fetch_the_dog(input)
   #implement your solution here
+  result = []
+  input.each_with_index do |animal, index|
+  	 result.push(animal) if index == 2
+  end
+  return result
 end
 
 def no_dogs_allowed(input)
   #implement your solution here
+  result = []
+  input.each_with_index do |animal, index|
+  	result.push(animal) if index != 2
+  end
+  return result
 end
 
 def count_the_animals(input)
   #implement your solution here
+  return input.length
+ 
 end
 
 def fetch_the_first_two(input)
   #implement your solution here
+  result = []
+  input.each_with_index do |animal, index|
+  	result.push(animal) if index < 2
+  end
+  return result
 end
 
 def fetch_CD_animals(input)
   #implement your solution here
+  final = []
+  result = []
+  input.each do |i|
+  	a = i.split(//)
+  	if(a[0] == 'c' or a[0] == 'd')
+  		result = a.join
+  		final.push(result)
+  	end
+  end
+  return final
 end
 
-## DO NOT CHANGE CODE BELOW THIS LINE ##
+
+
+# DO NOT CHANGE CODE BELOW THIS LINE ##
 
 animals = ["cat", "moose", "dog", "bird"]
 
