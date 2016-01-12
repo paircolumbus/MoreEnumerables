874dd55940e3c12774fc36e65bfe570a9a8a67db
Steve Kirby <skirby@covermymeds.com>
2016-01-12 13:21:45 -0500

    More Enumerables completed by skirby

--- challenge.rb
+++ challenge.rb
@@ -1,26 +1,27 @@
 
 def capitalize_each_string(input)
-  #implement your solution here
+  input.map { |x| x.capitalize }
 end
 
 def fetch_the_dog(input)
-  #implement your solution here
+  input.select { |x| x == "dog" }
 end
 
 def no_dogs_allowed(input)
-  #implement your solution here
+  input.find_all { |x| x != "dog" }
 end
 
 def count_the_animals(input)
-  #implement your solution here
+  input.length
 end
 
 def fetch_the_first_two(input)
-  #implement your solution here
+  input.take(2)
 end
 
 def fetch_CD_animals(input)
-  #implement your solution here
+  parts = input.partition { |x| x.start_with?("c", "d") }
+  parts[0]
 end
 
 ## DO NOT CHANGE CODE BELOW THIS LINE ##
